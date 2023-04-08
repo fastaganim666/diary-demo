71c30519600a8d7713c92231d709e37d098f7edf
fastaganim666 <fastaganim666@gmail.com>
2023-04-08 17:51:09 +0300

    allowed_hosts

--- project/settings.py
+++ project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-y69g8o6vs#odb(q&wv-a@6fr+)t)wkc7!$77pqo3hmh&u$6$yt
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['zapishitseli.ru']
 
 SITE_ID = 1
 
